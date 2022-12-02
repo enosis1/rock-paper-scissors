7eacbff89feab49101b7e17b9e31a46e8ba06ca3
Mike J <mtjohns98@gmail.com>
2022-12-01 20:34:31 -0500

    Add function singleRound to script.js

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function getComputerChoice() {
 	}
 	return computerChoice.toLowerCase();
 }
+
+function singleRound(playerSelection, computerChoice) {
+	computerChoice = getComputerChoice();
+	playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+
+	if (playerSelection === computerChoice) {
+		return ("Tie game!");
+	} else if (playerSelection === 'rock' && computerChoice === 'scissors') {
+		return ("You win! Rock beats Scissors!");
+	} else if (playerSelection === 'scissors' && computerChoice === 'paper') {
+		return ("You win! Scissors beats paper!");
+	} else if (playerSelection === 'paper' && computerChoice === 'rock') {
+		return ("You win! Rock beats paper!")
+	} else if (computerChoice === 'rock' && playerSelection === 'scissors') {
+		return ("You lose! Rock beats Scissors!");
+	} else if (computerChoice === 'scissors' && playerSelection === 'paper') {
+		return ("You lose! Scissors beats paper!");
+	} else if (computerChoice === 'paper' && playerSelection === 'rock') {
+		return ("You lose! Rock beats paper!")
+	}
+}
