97f37d9b7a1be1b93ca52e73476be8fe5e170f89
Mike J <mtjohns98@gmail.com>
2022-12-01 21:57:12 -0500

    Add function game to script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log('Hello World');
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
 	let computerChoice;
@@ -21,16 +23,39 @@ function singleRound(playerSelection, computerChoice) {
 	if (playerSelection === computerChoice) {
 		return ("Tie game!");
 	} else if (playerSelection === 'rock' && computerChoice === 'scissors') {
+		playerScore++
 		return ("You win! Rock beats Scissors!");
 	} else if (playerSelection === 'scissors' && computerChoice === 'paper') {
+		playerScore++
 		return ("You win! Scissors beats paper!");
 	} else if (playerSelection === 'paper' && computerChoice === 'rock') {
+		playerScore++
 		return ("You win! Rock beats paper!")
 	} else if (computerChoice === 'rock' && playerSelection === 'scissors') {
+		computerScore++
 		return ("You lose! Rock beats Scissors!");
 	} else if (computerChoice === 'scissors' && playerSelection === 'paper') {
+		computerScore++
 		return ("You lose! Scissors beats paper!");
 	} else if (computerChoice === 'paper' && playerSelection === 'rock') {
+		computerScore++
 		return ("You lose! Rock beats paper!")
+	}
+	
+}
+
+function game() {
+	for (i = 0; i < 5; i++) {
+		console.log(singleRound());
+		console.log(`Player Score: ${playerScore}.`);
+		console.log(`Computer Score: ${computerScore}.`);
+	}
+
+	if (playerScore === computerScore) {
+		console.log("Final Score: Tie.")
+	} else if (playerScore > computerScore) {
+		console.log("You win!")
+	} else {
+		console.log("Computer wins!")
 	}
 }
