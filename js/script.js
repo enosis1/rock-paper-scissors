9cb2aacf9170d77742d286cbfbd8c8e6a0abe93b
Mike Johnson <mtjohns98@gmail.com>
2023-09-05 11:37:15 -0400

    Update script.js
    I have finished the game() function. It asks the users for a set amount
    of rounds before the loop finishes.

--- js/script.js
+++ js/script.js
@@ -23,33 +23,66 @@ function getComputerChoice() {
   }
 }
 
-// Write a function that plays a single round of rock paper scissors
-//    function playSingleRound(playerSelection, computerSelection)
-function playSingleRound(playerSelection, computerSelection) {
-  // Returns user input and lower cases it
-  let playerSelection = playerSelection.toLowerCase();
-
-  if (playerSelection === computerSelection) {
-    return "Tie game!";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return `Player has won this round! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return `Player has won this round! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return `Player has won this round! ${playerSelection} beats ${computerSelection}`;
-  } else if (computerSelection === "paper" && playerSelection === "rock") {
-    return `Computer has won this round! ${computerSelection} beats ${playerSelection}`;
-  } else if (computerSelection === "rock" && playerSelection === "scissors") {
-    return `Computer has won this round! ${computerSelection} beats ${playerSelection}`;
-  } else if (computerSelection === "scissors" && playerSelection === "paper") {
-    return `Computer has won this round! ${computerSelection} beats ${playerSelection}`;
-  }
-}
-//    Return a string that declares the winner of the round
-//    Make the playerSelection case-insensative ('rock' 'ROck' 'ROCK')
-
 // Create a score tracker
 // Display score after each game (best of 5)
 // function game()
-// to play a 5 round game of RPS.
-// Within the game() function, get input from the user for playerSelection using prompt
+function game(rounds) {
+  // I need to keep score of the winners
+  let playerScore = 0;
+  let computerScore = 0;
+
+  // Loop through rounds
+  for (let i = 1; i <= rounds; i++) {
+    // Within the game() function, get input from the user for playerSelection using prompt
+    let playerSelection = prompt("Rock, Paper, or scissors?", "rock");
+    playSingleRound(playerSelection, getComputerChoice());
+  }
+
+  // Write a function that plays a single round of rock paper scissors
+  function playSingleRound(playerSelection, computerSelection) {
+    // Returns user input and lower cases it
+    // Make the playerSelection case-insensative ('rock' 'ROck' 'ROCK')
+    playerSelection = playerSelection.toLowerCase();
+
+    // Return a string that declares the winner of the round
+    if (playerSelection === computerSelection) {
+      return console.log("Tie game!");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+      playerScore++;
+      return console.log(
+        `Player has won this round! ${playerSelection} beats ${computerSelection} Current Player Score: ${playerScore}`
+      );
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      playerScore++;
+      return console.log(
+        `Player has won this round! ${playerSelection} beats ${computerSelection} Current Player Score: ${playerScore}`
+      );
+    } else if (
+      playerSelection === "scissors" &&
+      computerSelection === "paper"
+    ) {
+      playerScore++;
+      return console.log(
+        `Player has won this round! ${playerSelection} beats ${computerSelection} Current Player Score: ${playerScore}`
+      );
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+      computerScore++;
+      return console.log(
+        `Computer has won this round! ${computerSelection} beats ${playerSelection} Current Computer Score: ${computerScore}`
+      );
+    } else if (computerSelection === "rock" && playerSelection === "scissors") {
+      computerScore++;
+      return console.log(
+        `Computer has won this round! ${computerSelection} beats ${playerSelection} Current Computer Score: ${computerScore}`
+      );
+    } else if (
+      computerSelection === "scissors" &&
+      playerSelection === "paper"
+    ) {
+      computerScore++;
+      return console.log(
+        `Computer has won this round! ${computerSelection} beats ${playerSelection} Current Computer Score: ${computerScore}`
+      );
+    }
+  }
+}
