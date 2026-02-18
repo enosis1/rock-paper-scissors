cf83cd0de8ae58465ce7cfb7bfdd3663b9fc2134
Mike J <72291166+enosis1@users.noreply.github.com>
2026-02-18 15:28:14 -0500

    Implemenent getComputerChoice function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+// Write the logic to get the computer's choice (Between Rock, Paper and Scissors)
+// - Get strings "rock", "paper" or "scissors" randomly for the computer
+// - Return the value
+//
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  if (randomNumber === 1) {
+    return "rock";
+  } else if (randomNumber === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
+// Write the logic to get the human's choice (Between Rock, Paper and Scissors)
+// Declare the players score
+// Write the logic to play a single round
+// Write the logic to play the entire game out of 5 rounds
